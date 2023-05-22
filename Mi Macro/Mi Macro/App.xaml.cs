7cd94065b389096e4cc01b28cedc1f73fb2d8788
Jerson Rai Miranda Diaz <miranda.diaz.jerson.rai@outlook.com>
2023-05-22 09:43:35 -0600

    Add project files.

---
+++ Mi Macro/Mi Macro/App.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Mi_Macro.view;
+using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Mi_Macro
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new VLogin();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Mi Macro/Mi Macro/model/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Mi_Macro.model
+{
+    public class User
+    {
+        public string firsName { get; set; }
+        public string lastName { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+
+    }
+}
